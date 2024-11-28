094b30fc159e3f77e45bd19f852efb406bbbcf31
Tiavina Ratovonanahary <tiavinaratovonanahary@gmail.com>
2024-11-28 14:55:17 +0100

    complete update user profile page functionnality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,12 +1,52 @@
-import { Button, TextInput } from 'flowbite-react'
+import { Alert, Button, TextInput } from 'flowbite-react'
 import { useSelector } from 'react-redux'
+import {updateStart,updateSuccess,updateFailure} from '../redux/user/userSlice'
+import { useDispatch } from 'react-redux'
+import { useState } from 'react';
 
 export default function DashProfile() {
     const { currentUser } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const [formData, setFormData] = useState({});
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setUpdateUserError(null);
+      setUpdateUserSuccess(null);
+      if (Object.keys(formData).length === 0) {
+        setUpdateUserError('No changes made');
+        return ;
+      }
+      try {
+        dispatch(updateStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          dispatch(updateFailure(data.message));
+          setUpdateUserError(data.message);
+        } else {
+          dispatch(updateSuccess(data));
+          setUpdateUserSuccess("User's profile updated successfully");
+        }
+      } catch (error) {
+        dispatch(updateFailure(error.message));
+        setUpdateUserError(error.message);
+      }
+    }
     return (
         <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-        <form className='flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
           <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
             <img
               src={currentUser.profilePicture}
@@ -19,14 +59,16 @@ export default function DashProfile() {
             id='username'
             placeholder='username'
             defaultValue={currentUser.username}
+            onChange={handleChange}
           />
           <TextInput
             type='email'
             id='email'
             placeholder='email'
             defaultValue={currentUser.email}
+            onChange={handleChange}
           />
-          <TextInput type='password' id='password' placeholder='password' />
+          <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
           <Button type='submit' gradientDuoTone='purpleToBlue' outline>
               Update
           </Button>
@@ -35,6 +77,16 @@ export default function DashProfile() {
           <span className='cursor-pointer'>Supprimer le compte</span>
           <span className='cursor-pointer'>Se deconnecter</span>
         </div>
+        {updateUserSuccess && (
+          <Alert color='success' className='mt-5'>
+            {updateUserSuccess}
+          </Alert>
+        )}
+          {updateUserError && (
+          <Alert color='failure' className='mt-5'>
+            {updateUserError}
+          </Alert>
+        )}
       </div>
     );
   }
