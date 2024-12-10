6963f5be96cf6d2256106ac1bb736e0ef4f6f695
Tiavina Ratovonanahary <tiavinaratovonanahary@gmail.com>
2024-12-10 16:30:42 +0100

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
+import Search from './pages/Search'
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
         <Route path='/about' element={<About />}></Route>
         <Route path='/sign-in' element={<Signin />}></Route>
         <Route path='/sign-up' element={<Signup />}></Route>
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
